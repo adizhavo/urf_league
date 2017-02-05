5f4565148b8e6ef7d0d4bc370ee26a88ad09c848
Adi Zhavo <adi@gram.gs>
2017-02-05 15:18:41 +0000

    Add entity component

---
+++ urf_league/Assets/Source/Game/Entity/EntityComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace URFLeague.Game.Entity
+{
+    public abstract class EntityComponent : IEntity 
+    {
+        private IEntity parent;
+
+        public EntityComponent(IEntity parent)
+        {
+            if (parent == null) 
+                throw new ArgumentNullException("Parent entity", "Parent cennot be null");
+
+            this.parent = parent;
+        }
+
+        #region IEntity implementation
+
+        public IEntityData data
+        {
+            get { return parent.data; }
+        }
+
+        public abstract void Boot();
+
+        public abstract void Awake();
+
+        public abstract void FrameFeed();
+
+        public abstract void Destroy();
+
+        #endregion
+    }
+}
