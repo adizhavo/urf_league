9307e6ea5a9053fed4029f5ebe8e26d754ca7bf8
Adi Zhavo <adi@gram.gs>
2017-02-05 15:18:59 +0000

    Add champion data

---
+++ urf_league/Assets/Source/Game/Entity/Champion/ChampionData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace URFLeague.Game.Entity
+{
+    public class ChampionData : IEntityData
+    {
+        public string modelPath;
+        public GameObject gameObject;
+        public Vector3 worldPosition;
+    }
+}
