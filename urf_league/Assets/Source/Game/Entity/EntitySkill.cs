5cf0cc0022c36c642ec408ea35d841e4fc680502
Adi Zhavo <adi@gram.gs>
2017-02-05 17:54:53 +0000

    Add entity skill

---
+++ urf_league/Assets/Source/Game/Entity/EntitySkill.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace URFLeague.Game.Entity.Skill
+{
+    public interface ISkillData : IEntityData
+    {
+        string id {set; get;}
+        string[] disableComponentId {set; get;}
+    }
+
+    public abstract class EntitySkill : IEntity 
+    {
+        protected IEntity parent;
+
+        public EntitySkill(IEntity parent)
+        {
+            if (parent == null) 
+                throw new ArgumentNullException("Parent entity", "Parent cannot be null");
+
+            this.parent = parent;
+        }
+
+        #region IEntity implementation
+
+        public IEntityData data {get;}
+
+        public abstract void Boot();
+
+        public abstract void Awake();
+
+        public abstract void FrameFeed();
+
+        public abstract void Destroy();
+
+        #endregion
+    }
+}
