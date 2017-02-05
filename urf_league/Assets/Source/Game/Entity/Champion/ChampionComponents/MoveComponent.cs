aae1dcdb0a4d82bbc67715904ead866f0999a05b
Adi Zhavo <adi@gram.gs>
2017-02-05 16:49:11 +0000

    Add move component

---
+++ urf_league/Assets/Source/Game/Entity/Champion/ChampionComponents/MoveComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using URFLeague.Config;
+
+namespace URFLeague.Game.Entity.Component
+{
+    public class MoveComponent : EntityComponent
+    {
+        private ChampionData cd;
+
+        public MoveComponent(IEntity parent) : base(parent) { }
+
+        #region implemented abstract members of EntityComponent
+        public override void Boot()
+        {
+            if (base.data == null)
+                throw new NullReferenceException("Entity data is null");
+
+            cd = ((ChampionData)base.data);
+
+            if (cd == null)
+                throw new InvalidCastException("IEntity data is not a champion data");
+        }
+
+        public override void Awake() { }
+
+        public override void FrameFeed()
+        {
+            WorldCoordinate lookDirection = cd.targetPosition - cd.currentPosition;
+            if (lookDirection.toVector3().sqrMagnitude > Mathf.Epsilon)
+            {
+                cd.orientation = lookDirection.Normalized();
+                cd.currentPosition += cd.orientation * cd.movementSpeed * Time.deltaTime;
+            }
+        }
+
+        public override void Destroy() { }
+        #endregion
+    }
+}
