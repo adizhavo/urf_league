db2f54c8b807ad1877cde89a0c337736316d61af
Adi Zhavo <adi@gram.gs>
2017-02-05 16:03:43 +0000

    Add components factory

---
+++ urf_league/Assets/Source/Game/Factories/EntityComponentFactory.cs
@@ -0,0 +1,21 @@
+﻿using URFLeague.Config;
+using URFLeague.Game.Entity;
+
+namespace URFLeague.Game.Factory
+{
+    public static class EntityComponentFactory 
+    {
+        public static EntityComponent RequestComponent(string componentId, IEntity attachedTo)
+        {
+            switch (componentId)
+            {
+                case DataConfig.CHAMP_REND_COMP : 
+                    return new PositionComponent(attachedTo);
+                
+                default :
+                    UnityEngine.Debug.LogError(componentId + " component requested is not registered in the factory, will return null.");
+                    return null;
+            }
+        }
+    }
+}
