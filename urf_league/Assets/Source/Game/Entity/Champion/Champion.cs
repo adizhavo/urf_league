3dce6f220973ef33379ea1709dad6b384186a119
Adi Zhavo <adi@gram.gs>
2017-02-05 15:19:08 +0000

    Add champion

---
+++ urf_league/Assets/Source/Game/Entity/Champion/Champion.cs
@@ -0,0 +1,41 @@
+﻿namespace URFLeague.Game.Entity
+{
+    public class Champion : IEntity
+    {
+        private ChampionData championStats;
+
+        private EntityComponent[] championComponents;
+
+        #region IEntity implementation
+
+        public IEntityData data 
+        {
+            get { return championStats; }
+        }
+
+        public void Boot()
+        {
+            foreach(var cmp in championComponents)
+                cmp.Boot();
+        }
+
+        public void Awake()
+        {
+            foreach(var cmp in championComponents)
+                cmp.Awake();
+        }
+
+        public void FrameFeed()
+        {
+            foreach(var cmp in championComponents)
+                cmp.FrameFeed();
+        }
+
+        public void Destroy()
+        {
+            foreach(var cmp in championComponents)
+                cmp.Destroy();
+        }
+        #endregion
+    }
+}
