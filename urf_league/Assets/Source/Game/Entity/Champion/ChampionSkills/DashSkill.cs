6ab1cf299ffc85c90d7d57b11f58dd4655f4a258
Adi Zhavo <adi@gram.gs>
2017-02-05 21:06:27 +0000

    Add sample dash skill

---
+++ urf_league/Assets/Source/Game/Entity/Champion/ChampionSkills/DashSkill.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace URFLeague.Game.Entity.Attachable.Skill
+{
+    [Serializable]
+    public class DashData : AttachableEntityData
+    {
+        public string[] disableComponentId;
+        public float distance;
+        public float preparationTime;
+        public float recoveryTime;
+    }
+
+    public class Dash : Attachable<DashData, ChampionData> 
+    {
+        private ChampionData cd;
+
+        #region implemented abstract members of Attachable
+
+        public override void Awake()
+        {
+            
+        }
+
+        public override void FrameFeed()
+        {
+            
+        }
+
+        public override void Destroy()
+        {
+            
+        }
+
+        #endregion
+    }
+}
