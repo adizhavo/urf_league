d8f34a780ed2d9a48d1a750397b893e600ec165e
Adi Zhavo <adi@gram.gs>
2017-02-05 15:19:27 +0000

    Add champion position component

---
+++ urf_league/Assets/Source/Game/Entity/Champion/ChampionComponents/ChampionPositionComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace URFLeague.Game.Entity
+{
+    public class ChampionPositionComponent : EntityComponent 
+    {
+        private ChampionData cd;
+
+        public ChampionPositionComponent(IEntity parent) : base(parent) { }
+
+        #region implemented abstract members of EntityComponent
+
+        public override void Boot()
+        {
+            ChampionData cd = ((ChampionData)base.data);
+
+            if (cd == null)
+                throw new InvalidCastException("IEntity data is not a champion data");
+        }
+
+        public override void Awake()
+        {
+            cd.gameObject = UnityEngine.Resources.Load<UnityEngine.GameObject>(cd.modelPath);
+        }
+
+        public override void FrameFeed()
+        {
+            cd.gameObject.transform.position = cd.worldPosition;
+        }
+
+        public override void Destroy()
+        {
+            UnityEngine.GameObject.Destroy(cd.gameObject);
+        }
+
+        #endregion
+    }
+}
