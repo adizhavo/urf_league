7690971aa4b50d3bb97b1a67b6f3db818da36573
Adi Zhavo <adi@gram.gs>
2017-02-06 18:59:33 +0000

    Update folder structure and data directories

--- Assets/Source/Config/GameConfig.cs
+++ Assets/Source/Config/GameConfig.cs
@@ -3,6 +3,8 @@
     public static class GameConfig
     {
         public const string TERRAIN_TAG = "Terrain";
+        public const string GAMEOBJECT_POOL_NAME = "Pool";
+        public const string GAMEOBJECT_POOL_FOLDER = "Config/";
 
         public const float MIN_TARGET_POS_DISTANCE = 0.1f;
     }
