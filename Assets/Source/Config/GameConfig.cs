308a8348ae0191f4d66e469c7fe1bf6a727d4aa2
Adi Zhavo <adi@gram.gs>
2017-02-06 19:49:35 +0000

    Update game config

--- Assets/Source/Config/GameConfig.cs
+++ Assets/Source/Config/GameConfig.cs
@@ -2,10 +2,17 @@
 {
     public static class GameConfig
     {
+        // Tags
         public const string TERRAIN_TAG = "Terrain";
+
+        // Scritpable Obects
         public const string GAMEOBJECT_POOL_NAME = "Pool";
         public const string GAMEOBJECT_POOL_FOLDER = "Config/";
 
+        // Aniamtion Standarts
+        public const string ANIMATION_SCALE_TAG = "SpeedScale";
+
+        // GamePlay
         public const float MIN_TARGET_POS_DISTANCE = 0.1f;
     }
 }
